e2372d4d13e22ed926dcc7dcd800301a9e97d454
unknown <punithmatli@gmail.com>
2020-07-01 13:30:07 +0530

    Created Button component and added styles

--- front-end/src/places/pages/UserPlaces.js
+++ front-end/src/places/pages/UserPlaces.js
@@ -1,36 +1,39 @@
 import React from 'react';
+import { useParams } from 'react-router-dom'
 
 import PlaceList from '../components/PlaceList'
 
 const DUMMY_PLACES = [
     {
-        id:'p1',
-        title:'Empire State Building',
-        description:'One of the most famous sky scrapers in the world',
-        imageUrl:'https://untappedcities.com/wp-content/uploads/2015/07/Flatiron-Building-Secrets-Roof-Basement-Elevator-Sonny-Atis-GFP-NYC_5.jpg',
-        address:'20 W 34th St, New York, NY 10001, United States',
-        location:{
-           lat: 40.7484405,
-           lng: -73.9856644
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world',
+        imageUrl: 'https://untappedcities.com/wp-content/uploads/2015/07/Flatiron-Building-Secrets-Roof-Basement-Elevator-Sonny-Atis-GFP-NYC_5.jpg',
+        address: '20 W 34th St, New York, NY 10001, United States',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9856644
         },
-        creator:'u1'
+        creator: 'u1'
     },
     {
-        id:'p2',
-        title:'Empire State Building',
-        description:'One of the most famous sky scrapers in the world',
-        imageUrl:'https://untappedcities.com/wp-content/uploads/2015/07/Flatiron-Building-Secrets-Roof-Basement-Elevator-Sonny-Atis-GFP-NYC_5.jpg',
-        address:'20 W 34th St, New York, NY 10001, United States',
-        location:{
-           lat: 40.7484405,
-           lng: -73.9856644
+        id: 'p2',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world',
+        imageUrl: 'https://untappedcities.com/wp-content/uploads/2015/07/Flatiron-Building-Secrets-Roof-Basement-Elevator-Sonny-Atis-GFP-NYC_5.jpg',
+        address: '20 W 34th St, New York, NY 10001, United States',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9856644
         },
-        creator:'u2'
+        creator: 'u2'
     }
 ]
 
 const UserPlaces = () => {
-    return <PlaceList items={DUMMY_PLACES} />
+    const userId = useParams().userId;
+    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId)
+    return <PlaceList items={loadedPlaces} />
 }
 
 export default UserPlaces

526def2d6d2ce277f24e73bb434a347ee66ce284
unknown <punithmatli@gmail.com>
2020-08-05 14:37:45 +0530

    Added User Routes (Signup, Login, Getusers)

--- back-end/controllers/places-controllers.js
+++ back-end/controllers/places-controllers.js
@@ -42,17 +42,17 @@ const getPlaceById = (req, res, next) => {
     res.json({ place });
 }
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
     const userId = req.params.uid;
-    const place = DUMMY_PLACES.find(u => {
+    const places = DUMMY_PLACES.filter(u => {
         return u.creator === userId;
     })
 
-    if (!place) {
-        return next(new HttpError('Could not find a place for the provided user id', 404));
+    if (!places || places.length === 0) {
+        return next(new HttpError('Could not find places for the provided user id', 404));
     }
 
-    res.json({ place });
+    res.json({ places });
 }
 
 const createPlace = (req, res, next) => {
@@ -88,11 +88,11 @@ const updatePlace = (req, res, next) => {
 const deletePlace = (req, res, next) => {
     const placeId = req.params.pid;
     DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
-    res.status(200).json({message: "Deleted Place"})
+    res.status(200).json({ message: "Deleted Place" })
 }
 
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
 exports.updatePlace = updatePlace;
 exports.deletePlace = deletePlace;

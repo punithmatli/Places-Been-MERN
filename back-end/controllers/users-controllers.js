ffe3e7000f33c7a2768cae6ea53e422014f439fa
unknown <punithmatli@gmail.com>
2020-08-07 14:31:52 +0530

    Implemented Validation of API input.

--- back-end/controllers/users-controllers.js
+++ back-end/controllers/users-controllers.js
@@ -1,4 +1,5 @@
 const uuid = require('uuid')
+const { validationResult } = require('express-validator')
 
 const HttpError = require('../models/http-error')
 
@@ -16,34 +17,39 @@ const getUsers = (req, res, next) => {
 }
 
 const signup = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        throw new HttpError('Invalid inputs passed. Please check your data.', 422)
+    }
+
     const { name, email, password } = req.body;
 
     const hasUser = DUMMY_USERS.find(u => u.email === email)
-    if(hasUser) {
+    if (hasUser) {
         throw new HttpError('Could not create user. email already exists.', 422);
     }
 
     const createdUser = {
         id: uuid.v4(),
-        name, 
+        name,
         email,
         password
     };
 
     DUMMY_USERS.push(createdUser);
 
-    res.status(201).json({user: createdUser});
+    res.status(201).json({ user: createdUser });
 }
 
 const login = (req, res, next) => {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
     const identifiedUser = DUMMY_USERS.find(u => u.email === email);
-    if(!identifiedUser || identifiedUser.password !== password) {
+    if (!identifiedUser || identifiedUser.password !== password) {
         throw new HttpError('Could not identify user, credentials seem to be wrong.', 401);
     }
 
-    res.json({message: 'Logged in'})
+    res.json({ message: 'Logged in' })
 }
 
 exports.getUsers = getUsers;
--- back-end/routes/users-routes.js
+++ back-end/routes/users-routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check } = require('express-validator');
 
 const usersControllers = require('../controllers/users-controllers');
 const { route } = require('./places-routes');
@@ -7,7 +8,16 @@ const router = express.Router();
 
 router.get('/', usersControllers.getUsers);
 
-router.post('/signup', usersControllers.signup);
+router.post('/signup', [
+    check('name')
+        .not()
+        .isEmpty(),
+    check('email')
+        .normalizeEmail()
+        .isEmail(),
+    check('password')
+        .isLength({ min: 6 })
+], usersControllers.signup);
 
 router.post('/login', usersControllers.login);
 

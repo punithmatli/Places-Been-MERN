5329fef48768992b1bfebfed89b220b488fb2e2e
unknown <punithmatli@gmail.com>
2020-08-13 06:48:43 +0530

    Connected Backend to MongoDB using mongoose and Implemented Creation and Retrieval of documents from database

--- back-end/controllers/places-controllers.js
+++ back-end/controllers/places-controllers.js
@@ -3,6 +3,7 @@ const { validationResult } = require('express-validator')
 
 const HttpError = require('../models/http-error')
 const getCoordsForAddress = require('../util/location');
+const Place = require('../models/place')
 
 let DUMMY_PLACES = [
     {
@@ -31,17 +32,22 @@ let DUMMY_PLACES = [
     }
 ]
 
-const getPlaceById = (req, res, next) => {
+const getPlaceById = async (req, res, next) => {
     const placeId = req.params.pid;
-    const place = DUMMY_PLACES.find(p => {
-        return p.id === placeId
-    })
-
-    if (!place) {
-        throw new HttpError('Could not find a place for the provided id', 404);
+    let place;
+    try {
+        place = await Place.findById(placeId); //findBYId() does not return a promise but we can make it return one by writing it as findById().exec()
+    } catch (err) {
+        const error = new HttpError('Something went wrong, Could not find a place', 500);
+        return next(error);
     }
 
-    res.json({ place });
+    if (!place) {
+        const error = new HttpError('Could not find a place for the provided id', 404);
+        return next(error);
+    }
+
+    res.json({ place: place.toObject({getters: true}) });
 }
 
 const getPlacesByUserId = (req, res, next) => {
@@ -60,7 +66,7 @@ const getPlacesByUserId = (req, res, next) => {
 const createPlace = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-       return next(new HttpError('Invalid inputs passed. Please check your data.', 422))
+        return next(new HttpError('Invalid inputs passed. Please check your data.', 422))
     }
 
     const { title, description, address, creator } = req.body
@@ -72,16 +78,21 @@ const createPlace = async (req, res, next) => {
         return next(error);
     }
 
-    const createdPlace = {
-        id: uuid.v4(),
+    const createdPlace = new Place({
         title,
         description,
-        location: coordinates,
         address,
+        location: coordinates,
+        image: 'https://untappedcities.com/wp-content/uploads/2015/07/Flatiron-Building-Secrets-Roof-Basement-Elevator-Sonny-Atis-GFP-NYC_5.jpg',
         creator
-    }
+    });
 
-    DUMMY_PLACES.push(createdPlace);
+    try {
+        await createdPlace.save()
+    } catch (err) {
+        const error = new HttpError('Creating Place failed. Please try again', 500);
+        return next(error);
+    }
 
     res.status(201).json({ place: createdPlace });
 }
@@ -107,8 +118,8 @@ const updatePlace = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
     const placeId = req.params.pid;
-    if(!DUMMY_PLACES.find(p => p.id === placeId)) {
-        throw new HttpError('Could not find a place for that ID.',404);
+    if (!DUMMY_PLACES.find(p => p.id === placeId)) {
+        throw new HttpError('Could not find a place for that ID.', 404);
     }
     DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
     res.status(200).json({ message: "Deleted Place" })

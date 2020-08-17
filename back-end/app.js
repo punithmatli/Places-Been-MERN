8f223698167f2eea696c157db1a1d00f95f196ba
unknown <punithmatli@gmail.com>
2020-08-17 10:54:15 +0530

    Established Connection between Frontend and Backend and Implemented Signup.

--- back-end/app.js
+++ back-end/app.js
@@ -10,6 +10,13 @@ const app = express()
 
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+    next();
+})
+
 app.use('/api/places', placesRoutes);
 
 app.use('/api/users', usersRoutes);
@@ -28,7 +35,7 @@ app.use((error, req, res, next) => {
 })
 
 mongoose
-.connect('mongodb+srv://punith:mernproject@cluster0.amt34.mongodb.net/places?retryWrites=true&w=majority')
+.connect('mongodb+srv://punith:mernproject@cluster0.amt34.mongodb.net/mern?retryWrites=true&w=majority')
 .then(() => {
     app.listen(5000);
 })

816552ec37b36dd4c4d2a884796a2e73e14a8e16
unknown <punithmatli@gmail.com>
2020-08-18 16:03:21 +0530

    Adding Places using POST request

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -11,13 +11,16 @@ import { AuthContext } from './shared/context/auth-context'
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userId, setUserId] = useState();
 
-  const login = useCallback(() => {
-    setIsLoggedIn(true)
+  const login = useCallback((uid) => {
+    setIsLoggedIn(true);
+    setUserId(uid);
   }, [])
 
   const logout = useCallback(() => {
-    setIsLoggedIn(false)
+    setIsLoggedIn(false);
+    setUserId(null);
   }, [])
 
   let routes;
@@ -59,7 +62,12 @@ const App = () => {
 
   return (
     <AuthContext.Provider
-      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+      value={{
+        isLoggedIn: isLoggedIn,
+        userId: userId,
+        login: login,
+        logout: logout
+      }}
     >
       <Router>
         <MainNavigation />
--- front-end/src/user/pages/Auth.js
+++ front-end/src/user/pages/Auth.js
@@ -50,7 +50,7 @@ const Auth = () => {
 
 		if (isLoginMode) {
 			try {
-				await sendRequest(
+				const responseData = await sendRequest(
 					'http://localhost:5000/api/users/login',
 					'POST',
 					JSON.stringify({
@@ -61,13 +61,13 @@ const Auth = () => {
 						'Content-Type': 'application/json'
 					}
 				);
-				auth.login();
+				auth.login(responseData.user.id);
 			} catch (err) {
 
 			}
 		} else {
 			try {
-				await sendRequest(
+				const responseData = await sendRequest(
 					'http://localhost:5000/api/users/signup',
 					'POST',
 					JSON.stringify({
@@ -79,7 +79,7 @@ const Auth = () => {
 						'Content-Type': 'application/json'
 					}
 				);
-				auth.login();
+				auth.login(responseData.user.id);
 			} catch (err) {
 
 			}

9181ebdb868a3be422dc4b37dd5e8d8fbe3882a1
unknown <punithmatli@gmail.com>
2020-08-18 19:49:15 +0530

    Loading Places by User Id

--- front-end/src/places/pages/UserPlaces.js
+++ front-end/src/places/pages/UserPlaces.js
@@ -1,39 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom'
 
-import PlaceList from '../components/PlaceList'
+import PlaceList from '../components/PlaceList';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal'
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner'
+import { useHttpClient } from '../../shared/hooks/http-hook'
 
-const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world',
-        imageUrl: 'https://untappedcities.com/wp-content/uploads/2015/07/Flatiron-Building-Secrets-Roof-Basement-Elevator-Sonny-Atis-GFP-NYC_5.jpg',
-        address: '20 W 34th St, New York, NY 10001, United States',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9856644
-        },
-        creator: 'u1'
-    },
-    {
-        id: 'p2',
-        title: 'Charminar',
-        description: 'One of the most famous monuments in India',
-        imageUrl: 'https://i2.wp.com/natureconservation.in/wp-content/uploads/2018/03/95.jpg',
-        address: 'Hyderabad, India',
-        location: {
-            lat: 17.3615636,
-            lng: 78.4746645
-        },
-        creator: 'u1'
-    }
-]
+
+
 
 const UserPlaces = () => {
+    const [loadedPlaces, setLoadedPlaces] = useState();
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
     const userId = useParams().userId;
-    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId)
-    return <PlaceList items={loadedPlaces} />
+
+    useEffect(() => {
+        const fetchPlaces = async () => {
+            try {
+                const responseData = await sendRequest(
+                    `http://localhost:5000/api/places/user/${userId}`,
+                );
+                setLoadedPlaces(responseData.places);
+            } catch (err) { }
+        }
+        fetchPlaces();
+    }, [sendRequest, userId])
+
+    return <React.Fragment>
+        <ErrorModal error={error} onClear={clearError} />
+        {isLoading && <div className="center"><LoadingSpinner asOverlay /></div>}
+        {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+    </React.Fragment>
 }
 
 export default UserPlaces

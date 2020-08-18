6ea09497b868df196a32929179e74a41e91d8181
unknown <punithmatli@gmail.com>
2020-08-18 06:45:42 +0530

    Displayed users with GET Request to Backend REST API and Created custom http-hook to avoid code duplication.

--- front-end/src/user/pages/Users.js
+++ front-end/src/user/pages/Users.js
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import UsersList from '../components/UsersList'
+import ErrorModal from '../../shared/components/UIElements/ErrorModal'
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner'
 
 const Users = () => {
-    const USERS = [
-        {
-            id: 'u1',
-            name: 'Max',
-            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSD_6JIXq12IXBBxIMvynAeGKyVG2sdlNyoLw&usqp=CAU',
-            places: 3
-        }
-    ]
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
+    const [loadedUsers, setLoadedUsers] = useState();
 
-    return <UsersList items={USERS} />
+    useEffect(() => {
+        const sendRequest = async () => {
+            setIsLoading(false);
+            try {
+                const response = await fetch('http://localhost:5000/api/users');
+                const responseData = await response.json();
+
+                if (!response.ok) {
+                    throw new Error(responseData.message);
+                }
+
+                setLoadedUsers(responseData.users);
+                setIsLoading(false);
+            } catch (err) {
+                setIsLoading(false);
+                setError(err.message);
+            }
+
+        }
+        sendRequest();
+    }, [])
+
+    const errorHandler = () => {
+        setError(null);
+    }
+
+    return <React.Fragment>
+        <ErrorModal error={error} onClear={errorHandler} />
+        {isLoading && (<div className="center">
+            <LoadingSpinner />
+        </div>)}
+        {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
 }
 
 export default Users

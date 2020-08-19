b2fe2a2d874b245c2af1cdfa96ef6ad491aa67b0
unknown <punithmatli@gmail.com>
2020-08-19 21:37:11 +0530

    Improved Delete Places Functionality

--- front-end/src/places/components/PlaceList.js
+++ front-end/src/places/components/PlaceList.js
@@ -29,7 +29,8 @@ const PlaceList = props => {
             description={place.description}
             address={place.address}
             creatorId={place.creator}
-            coordinates={place.location} />)}
+            coordinates={place.location}
+            onDelete={props.onDeletePlace} />)}
     </ul>
 }
 
--- front-end/src/places/pages/UserPlaces.js
+++ front-end/src/places/pages/UserPlaces.js
@@ -6,9 +6,6 @@ import ErrorModal from '../../shared/components/UIElements/ErrorModal'
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner'
 import { useHttpClient } from '../../shared/hooks/http-hook'
 
-
-
-
 const UserPlaces = () => {
     const [loadedPlaces, setLoadedPlaces] = useState();
     const { isLoading, error, sendRequest, clearError } = useHttpClient();
@@ -27,10 +24,20 @@ const UserPlaces = () => {
         fetchPlaces();
     }, [sendRequest, userId])
 
+    const placeDeletedHandler = deletedPlaceId => {
+        setLoadedPlaces(prevPlaces =>
+            prevPlaces.filter(place => place.id !== deletedPlaceId)
+        )
+    }
+
     return <React.Fragment>
         <ErrorModal error={error} onClear={clearError} />
         {isLoading && <div className="center"><LoadingSpinner asOverlay /></div>}
-        {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+        {!isLoading && loadedPlaces && <PlaceList
+            items={loadedPlaces}
+            onDeletePlace={placeDeletedHandler}
+        />
+        }
     </React.Fragment>
 }
 

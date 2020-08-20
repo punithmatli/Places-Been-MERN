1305c301b2e5942f4d139753b95625a7334237dc
unknown <punithmatli@gmail.com>
2020-08-20 15:13:44 +0530

    Fixed MyPlaces Navigation link and Made Final adjustments

--- front-end/src/user/pages/Auth.js
+++ front-end/src/user/pages/Auth.js
@@ -116,8 +116,8 @@ const Auth = () => {
 						id="password"
 						type="password"
 						label="Password"
-						validators={[VALIDATOR_MINLENGTH(5)]}
-						errorText="Please enter a valid password, atleast 5 characters."
+						validators={[VALIDATOR_MINLENGTH(6)]}
+						errorText="Please enter a valid password, atleast 6 characters."
 						onInput={inputHandler}
 					/>
 					<Button
